e0aaa7cff51ec696b49019326534accb02788698
NguyenKhoa <khoabeoloidom@gmail.com>
2026-08-10 16:38:53 +0700

    Tối ưu lại code

--- obj/Debug/net9.0-android/POSPrinter.AssemblyInfo.cs
+++ obj/Debug/net9.0-android/POSPrinter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POSPrinter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b408a28e89a075719ba24cf5d486d82a493b6d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("POSPrinter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POSPrinter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
